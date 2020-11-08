797b97f1779272b9107685a794010646b1eb2f96
Junek3 <rlarudrms28@gmail.com>
2020-11-08 22:50:55 +0900

    Add Api-key to deploy app

--- src/api.js
+++ src/api.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
-const api_key = process.env.REACT_APP_API_KEY;
-
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
   params: {
-    api_key,
+    api_key: '68d01aa896ae6225fba194afa6db7d38',
     language: 'en-US',
   },
 });
